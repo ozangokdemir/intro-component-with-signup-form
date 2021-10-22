3abc6a1dca27f762d31ffece0256d2f6ff5816e2
Ozan GÖKDEMIR <ozan.gokdemir@outlook.fr>
2021-10-22 12:07:13 +0200

    Files recovery from ext computer
    Added a remarks.txt to change to .md to be read by github interface.
    Don't remember which changes I made since last update, but I think I had a problem about child node creation and suppression with js.

--- script.js
+++ script.js
@@ -7,15 +7,26 @@ document
     .getElementById('claim')
     .addEventListener('click', function() {
         const elt =  document.querySelectorAll('.field:not(:last-of-type)');
-        console.log('Taille : ', elt.length);
+        console.log('Premier élement : ', elt[1].firstElementChild);
         for (let i=0; i<elt.length; i++) {
             if (elt[i].value == '') {
-                elt[i].style.border = '2px solid hsl(0, 100%, 74%)';
+                elt[i].style.outline = '2px solid hsl(0, 100%, 74%)';
                 elt[i].style.background = 'url("images/icon-error.svg") no-repeat 95%';
-                elt[i].after(document.createElement('p'));
+
+                /* if (elt[i].firstElementChild) {
+                    elt[i].after(document
+                        .createElement('p')
+                        .appendChild(document.createTextNode(elt[i].getAttribute('placeholder') + ' cannot be empty')
+                    ));
+                } */
+
+                elt[i].appendChild(document.createElement('p'));
+
+                        
             }
             else {
                 elt[i].removeAttribute('style');
+                elt[i].removeChild(document.querySelector('.field p'));
             }
         }
     });
